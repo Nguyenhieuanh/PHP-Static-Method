4a8c964e09b414192a73d9c4a30bc12c5752dd23
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-05-24 15:49:44 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include "Class/Application.php";
+
+$app1 = Application::getInstance();
+$app2 = Application::getInstance();
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>[Thực hành] Tạo một đối tượng duy nhất của lớp</title>
+</head>
+<body>
+
+</body>
+</html>
